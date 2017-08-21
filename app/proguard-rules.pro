b9fbd64eb67f73ae1b9fe06107d04e6971c769b9
huzhenjie <huzhenjie.dev@gmail.com>
2017-08-21 11:16:01 +0800

    remove mi push

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,5 +1,4 @@
 -keep public class * extends android.content.BroadcastReceiver
--keep class com.scrat.app.bus.push.MiMessageReceiver {*;}
 
 -dontwarn okio.**
 
--- app/src/main/java/com/scrat/app/bus/BusApp.java
+++ app/src/main/java/com/scrat/app/bus/BusApp.java
@@ -1,13 +1,7 @@
 package com.scrat.app.bus;
 
-import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
-
-import com.scrat.app.bus.utils.Utils;
-import com.xiaomi.mipush.sdk.MiPushClient;
-
-import java.util.List;
 
 /**
  * Created by yixuanxuan on 16/4/19.
@@ -19,31 +13,13 @@ public class BusApp extends Application {
         return mApp.getApplicationContext();
     }
 
-    private static final String APP_ID = "2882303761517474662";
-    private static final String APP_KEY = "5551747492662";
-
     @Override
     public void onCreate() {
         super.onCreate();
         mApp = this;
-        if (shouldInit()) {
-            MiPushClient.registerPush(this, APP_ID, APP_KEY);
-            String alias = Utils.getAndroidID(mApp.getApplicationContext());
-            MiPushClient.setAlias(mApp.getApplicationContext(), alias, null);
+        if (BuildConfig.DEBUG) {
+            com.squareup.leakcanary.LeakCanary.install(this);
         }
-        com.squareup.leakcanary.LeakCanary.install(this);
     }
 
-    private boolean shouldInit() {
-        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
-        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
-        String mainProcessName = getPackageName();
-        int myPid = android.os.Process.myPid();
-        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
-            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
